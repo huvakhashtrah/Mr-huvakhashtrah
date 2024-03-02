3e46f45f5f8a2c8b4e0cd28e3870711e3c5b163d
huvakhashtrah <160272826+huvakhashtrah@users.noreply.github.com>
2024-03-02 21:26:51 +0330

    Add files via upload

---
+++ h-kh.py
@@ -0,0 +1,69 @@
+from termcolor import colored
+from colorama import Fore, Style
+text = """⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⣠⣴⣶⣦
+⢀⢀⢀⢀⢀⢀⢀⢀⢀⠠⣾⣿⣿⣿⣿⣿⣦
+⢀⢀⢀⢀⢀⢀⢀⢀⣠⣾⡿⠿⠿⠿⢿⣿⣷⣧
+⢀⢀⢀⢀⢀⢀⣠⣾⣿⠏⢀⢀⢀⢀⢀⠈⠻⣿⣷⡄
+⢀⢀⢀⢀⢀⣼⣿⣿⡏⢀⢀⢀⢀⢀⢀⢀⢀⢹⣿⣿⡀
+⢀⢀⢀⢀⢀⣿⣿⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⠘⣿⣿⡇
+⢀⢀⢀⢀⢀⢿⣿⣿⡇⢀⢀⢀⢀⢀⢀⢀⢀⢸⣿⣿⡇
+⢀⢀⢀⢀⢀⠘⣿⣿⣷⢀⢀⢀⢀⢀⢀⢀⢀⣿⣿⡿
+⢀⢀⢀⢀⢀⢀⠈⢿⣿⣦⢀⢀⢀⢀⢀⢀⣾⣿⣿⠃
+⢀⢀⢀⢀⢀⢀⢀⢀⣿⣿⣷⣄⢀⢀⢀⣼⣿⣿⡃
+⢀⢀⢀⢀⢀⢀⣀⣼⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⣷⣄
+⢀⢀⢀⣠⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣷⣦⣀
+⢀⣴⣿⣿⣿⣿⣿⣿⣿⡟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄
+⣼⣿⣿⣿⣿⣿⣿⣿⣿⢣⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟
+⢻⣿⣿⣿⣿⣿⣿⣿⣿⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇
+my channel = https://t.me/filter_rubika_81
+im = https://t.me/huvakhashtrah"""
+colored_text = colored(text, 'red')
+print(colored_text)
+
+def main():
+    print("1: site sex")
+    print("2: link dark")
+    print("3: link virus")
+    print("4: bug server")
+    print("5: github bug")
+    print("6: Algorithm")
+    print("7: link virus sensitive")
+    print("8: bug porn")
+    print("9: link sex")
+    print("10: github sexy")
+
+    choice = input("Choose an option 👉    ")
+
+    if choice == '1':
+        print(Fore.BLUE + " https://babatop.xyz/10501-v-krasivye-popki-seks-szadi-40-foto.html  | https://www.phwmtv.cloud")
+    elif choice == '2':
+        print(Fore.RED + "https://www.channel4.com/programmes/how-to-hire-a-hitman | https://www.hackread.com/doctor-bitcoin-dark-web-hitman/")
+    elif choice == '3':
+        print(Fore.YELLOW + " http://ffmodmenu.page.link/LatestMODMENU | http://ffmodmenu.page.link/Anti-V")
+    elif choice == '4':
+        print(Fore.CYAN + " http://Rubika.filter.yftt17.Xxnx.com/Af.aF.Af.Af.Af.Af.Af.Af.Af.Af.4.0.9.3.8.1.0.filtering.gif.sex.rubika.com.2.1.0.3.0     | https://github.com/huvakhashtrah/bug-rubika-report-huvakhashtrah/blob/main/Bg.Server-rubika")
+    elif choice == '5':
+        print(Fore.MAGENTA + "https://github.com/huvakhashtrah/bug-rubika-report-huvakhashtrah/blob/main/Bug-darkweb-hacking | https://github.com/huvakhashtrah/bug-rubika-report-huvakhashtrah/blob/main/hacker.user")
+    elif choice == '6':
+        print(Fore.LIGHTBLACK_EX + " https://4.0.7.5.fil.fil.fil/85k/Filtering_rubika/h.2.4.0.1.9.0.4.5.6.0.2.9.poren.3.9.2.3.6.7.0.1.Fil/Virus/ly.310.561  | http://Porn.com/V.Q.K.4/f/h.u//6.9.1.1.1.1.8.6.8.9.7.9.8.9.9.5/")
+    elif choice == '7':
+        print(Fore.GREEN + "Virus.org/ffmodmenu.page.link/Anti-V(Sends.a.viral.link)/viru3.com")
+    elif choice == '8':
+        print(Fore.LIGHTBLACK_EX + " https://github.com/huvakhashtrah/bug-rubika-report-huvakhashtrah/blob/main/Bug.porn.in.rubika.io | bug-filtering-user=xnxx.com")
+    elif choice == '9':
+        print(Fore.GREEN + "https://uploadkon.ir/uploads/479307_24lucy-doll-5.jpg | https://uploadkon.ir/uploads/266225_246ca9b1803e2f700243c407965fc9cd81-25.jpg")
+    elif choice == '10':
+        print("https://github.com/huvakhashtrah/bug-rubika-report-huvakhashtrah/blob/main/266225_246ca9b1803e2f700243c407965fc9cd81-25.jpg | https://github.com/huvakhashtrah/bug-rubika-report-huvakhashtrah/blob/main/479307_24lucy-doll-5.jpg")
+    else:
+        print("The entered option is not valid!")
+
+if __name__ == "__main__":
+    main()
+    
+    import pyfiglet
+from termcolor import colored
+
+text = "[Program finished]"
+result = pyfiglet.figlet_format(text, font="slant")
+colored_result = colored(result, color="green")
+print(colored_result)
